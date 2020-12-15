fd86677bc3a81cf03d79e2569203b3418b60ef57
Alvy <floressuarezalvaro@gmail.com>
2020-12-14 20:53:59 -0800

    added js to html routes

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -11,4 +11,8 @@ router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
     });
 
+router.get("/assets/js/index.js", (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/assets/js/index.js"));
+    })
+
 module.exports = router;
