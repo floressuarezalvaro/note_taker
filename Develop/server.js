a4d1538b9720a1e441ef338796954e67b7f9f4b7
Alvy <floressuarezalvaro@gmail.com>
2020-12-14 20:10:56 -0800

    updates routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,16 @@
 const fs = require ('fs');
 const express = require ('express');
 const app = express();
+const apiRoutes = require("./routes/apiRoutes")
+const htmlRoutes = require("./routes/htmlRoutes")
 const PORT = 8080;
+
+app.use("/api", apiRoutes)
+app.use("/", htmlRoutes)
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
-require('./routes/apiRoutes')(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: http://localhost:${PORT}`)
